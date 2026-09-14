0b0c4e7c56a35fa5146fa6cb3b249605f599e8ec
Wada <aidanharvey64@gmail.com>
2026-09-14 15:55:32 -0400

    FirstSteps
    FirstStpes is some code I mmade with the freshies

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstSteps.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class FirstSteps extends OpMode {
+    private DcMotorEx gerald;
+    private DcMotor Jimothy;
+
+    @Override
+    public void init() {
+        gerald = hardwareMap.get(DcMotorEx.class, "gerald");
+        gerald.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        gerald.setDirection(DcMotorSimple.Direction.REVERSE);
+        Jimothy = hardwareMap.get(DcMotor.class, "christian");
+        Jimothy.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            gerald.setVelocity(1600);
+        }
+        if (gamepad1.b) {
+            Jimothy.setPower(0.5);
+        }
+        ;
+
+    }
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDrive.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
